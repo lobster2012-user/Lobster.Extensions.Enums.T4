64ba26a8ed7137ccdec28402b45bb04649c681ae
lobster2012-user <lobster2012.user@gmail.com>
2018-06-19 01:04:35 +0300

    1.0.19

---
+++ Lobster.Extensions.Enums.T4.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lobster.Extensions.Enums.T4.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
