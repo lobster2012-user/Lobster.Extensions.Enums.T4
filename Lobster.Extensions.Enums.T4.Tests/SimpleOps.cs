41b226b32c95dd91b855eba0ebebf08c304dc224
lobster2012-user <lobster2012.user@gmail.com>
2018-06-19 21:16:47 +0300

    add tests

---
+++ Lobster.Extensions.Enums.T4.Tests/SimpleOps.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Lobster.Extensions.Enums.T4.Tests
+{
+    [TestClass]
+    public class SimpleOps
+    {
+        [TestMethod]
+        public void Test_Set()
+        {
+            Assert.AreEqual(
+                FileAccess.Read.Set(FileAccess.Write),
+                FileAccess.ReadWrite);
+        }
+        [TestMethod]
+        public void Test_Clear()
+        {
+            Assert.AreEqual(
+                FileAccess.ReadWrite.Reset(FileAccess.Write),
+                FileAccess.Read);
+        }
+        [TestMethod]
+        public void Test_IsAnySet()
+        {
+            Assert.IsTrue(
+                (FileAccess.ReadWrite).IsAnySet(FileAccess.Read));
+            Assert.IsFalse(
+                (FileAccess.Write).IsAnySet(FileAccess.Read));
+        }
+        [TestMethod]
+        public void Test_AreAllSet()
+        {
+            Assert.IsFalse(
+                (FileAccess.Read).AreAllSet(FileAccess.ReadWrite));
+            Assert.IsTrue(
+                (FileAccess.ReadWrite).AreAllSet(FileAccess.ReadWrite));
+        }
+    }
+}
