53af404d3b75511c896dbae22e0d6797e47a510b
lobster2012-user <lobster2012.user@gmail.com>
2018-06-20 02:18:12 +0300

    1.0.20

---
+++ Lobster.Extensions.Enums.T4.Sample/generate_file_access_ops.cs
@@ -0,0 +1,17 @@
+﻿  
+
+
+  
+
+
+namespace Lobster.Extensions.Enums
+{
+	public static class Extensions_Enum_Ops
+	{
+
+		public static bool IsAnySet(this System.IO.FileAccess value, System.IO.FileAccess flags) => (value & flags) != 0;
+		public static bool AreAllSet(this System.IO.FileAccess value, System.IO.FileAccess flags) => (value & flags) == flags;
+		public static System.IO.FileAccess Set(this System.IO.FileAccess value, System.IO.FileAccess flags) => value | flags;
+		public static System.IO.FileAccess Reset(this System.IO.FileAccess value, System.IO.FileAccess flags) => value & ~flags;
+}
+	}
